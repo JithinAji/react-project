d9a02bfd864c2cad60ca73fd11c0660cb5809239
JithinAji <jithin_aji@outlook.com>
2021-05-20 01:09:37 +0530

    clear button set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import DataFetching from "./DataFetching";
 import "./App.css";
 
 function App() {
+  // const [value, setValue] = useState("");
+
+  // const updateInput = (e) => {
+  //   this.setValue({
+  //     value: e.target.value,
+  //   });
+  // };
+
+  const [value, setVal] = useState("");
+  const onChange = (e) => {
+    setVal(e.target.value);
+  };
+
+  const onClear = () => {
+    setVal("");
+  };
+
   return (
     <div>
       <div className="search">
-        <input type="text" /> <button>Search</button>
+        <input type="text" value={value} onChange={onChange} />
+        <button onClick={onClear}>Clear</button>
       </div>
 
       <DataFetching />
