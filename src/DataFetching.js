48ca9c8c94e462e77b933053e534203caec4ddac
JithinAji <jithin_aji@outlook.com>
2021-05-20 00:27:58 +0530

    data fetching done and added to the page

---
+++ src/DataFetching.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./DataFetching.css";
+
+function DataFetching() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        console.log(typeof result.data[0]);
+        setUsers(result.data);
+      } catch (error) {
+        // handle any GET request or response handling errors
+        console.log("fetch error");
+      }
+    };
+    fetchData();
+  }, []);
+
+  //if(!users) return 'loading...';
+
+  return (
+    <div>
+      <table className="usertable">
+        {users &&
+          users.map((user) => (
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+      </table>
+    </div>
+  );
+}
+
+export default DataFetching;
